edfc4a8843df37d293b72e3905020b2d5d4edefc
mikemeredith <mmeredith@wcs.org>
2014-09-20 15:29:49 +0800

    Changes to hms

--- R/hms.R
+++ R/hms.R
@@ -1,19 +1,25 @@
-hms <-
-function(x, char=FALSE) {
+# Function to convert time in seconds or a 'proc_time' object
+#   to hours, min, secs.
+# x is a vector of times in seconds, or a 'proc_time' object.
+# Returns:
+#  If char == 0, a matrix with named columns for hours, mins, secs
+#  If char == 1, a vector of strings of the form "6 mins 20 secs"
+#  If char == 2, a vector of strings of the form "00:06:20"
+
+hms <- function(x, char=0) {
   if(inherits(x, "proc_time"))
      x <- x[3]
-  t <- numeric(3)
-  t[1] <- x %/% 3600
   x1 <- x %% 3600
-  t[2] <- x1 %/% 60
-  t[3] <- round(x1 %% 60)
-  if(char) {
-    th <- if(t[1] > 0) paste(t[1], "hrs ") else NULL
-    tm <- if(t[2] > 0) paste(t[2], "mins ") else NULL
-    ts <- paste(t[3], "secs") 
-    return(paste(th, tm, ts, sep=""))
+  t <- cbind(hrs=x %/% 3600, mins=x1 %/% 60, secs=round(x1 %% 60))
+  if(char==1) {
+    tmp <- sprintf("%d hrs %d mins %d secs", t[, 1], t[, 2], t[, 3])
+    start <- ifelse(t[, 1] == 0, 
+                ifelse(t[, 2] == 0, 14, 7), 1)
+    out <- substr(tmp, start, 30)
+  } else  if (char==2) {
+    out <- sprintf("%02d:%02d:%02d", t[, 1], t[, 2], t[, 3])
   } else {
-     names(t) <- c("hrs", "mins", "secs")
-     return(t)
+     out <- t
   }
+  return(out)
 }
